f9b1c00feeef1e678030f9e75bdb45cb034d9c45
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-05-17 16:23:41 +0300

    Create layer models with entity Article

---
+++ src/CoreWiki.Models/Article.cs
@@ -0,0 +1,22 @@
+﻿namespace CoreWiki.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Article
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Topic { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime PublishedOn { get; set; } 
+
+        
+    }
+}
