1ca2d073b041a16e2c0cbe0b19b9f72edf729d6e
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-05-17 16:24:11 +0300

    Create Data layer

---
+++ src/CoreWiki.Data/CoreWikiContext.cs
@@ -0,0 +1,21 @@
+﻿namespace CoreWiki.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class CoreWikiContext : DbContext
+    {
+        public CoreWikiContext()
+        {
+            
+        }
+
+        public CoreWikiContext(DbContextOptions<CoreWikiContext> options)
+            :base(options)
+        {
+            
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
