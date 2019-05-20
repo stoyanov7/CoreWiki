4e932fdd92d01de1fa876015bfd636aff59bc9a5
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-05-20 16:13:35 +0300

    Create LatestChanges page

---
+++ src/CoreWiki.Web/Pages/Article/LatestChanges.cshtml.cs
@@ -0,0 +1,31 @@
+﻿namespace CoreWiki.Web.Pages.Article
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Data;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class LatestChangesModel : PageModel
+    {
+        private readonly CoreWikiContext context;
+
+        public LatestChangesModel(CoreWikiContext context)
+        {
+            this.context = context;
+        }
+
+        public IList<Article> Articles { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            this.Articles = await this.context
+                .Articles
+                .OrderByDescending(x => x.PublishedOn)
+                .Take(5)
+                .ToListAsync();
+        }
+    }
+}
