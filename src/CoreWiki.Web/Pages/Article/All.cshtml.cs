11610210e4089f7ce8873898aee203bf9dbdf483
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-05-22 14:10:27 +0300

    Add an All articles page #4

---
+++ src/CoreWiki.Web/Pages/Article/All.cshtml.cs
@@ -0,0 +1,48 @@
+﻿namespace CoreWiki.Web.Pages.Article
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Data;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class AllModel : PageModel
+    {
+        private readonly CoreWikiContext context;
+        private const int PageSize = 2;
+
+        public AllModel(CoreWikiContext context)
+        {
+            this.context = context;
+        }
+
+        [FromQuery()]
+        public int PageNumber { get; set; } = 1;
+
+        public int TotalPages { get; set; }
+
+        public IEnumerable<Article> Articles { get; set; }
+
+        public async Task OnGet()
+        {
+            this.Articles = await this.context
+                .Articles
+                .AsNoTracking()
+                .OrderBy(a => a.Topic)
+                .Skip((this.PageNumber - 1) * PageSize)
+                .Take(PageSize)
+                .ToArrayAsync();
+
+            var count = await this.context
+                .Articles
+                .CountAsync();
+
+            this.TotalPages = (int)Math.Ceiling(count / (double)PageSize);
+
+        }
+    }
+}
