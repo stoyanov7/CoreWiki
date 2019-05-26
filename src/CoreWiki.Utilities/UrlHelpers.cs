468685f20be28c6f34f8f8a0810ffc2f51520dd7
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-05-26 12:30:48 +0300

    Reduce the nesting if statements in method, use regular expression to process the slug

--- src/CoreWiki.Utilities/UrlHelpers.cs
+++ src/CoreWiki.Utilities/UrlHelpers.cs
@@ -1,155 +1,48 @@
 ﻿namespace CoreWiki.Utilities
 {
+    using System.Globalization;
     using System.Text;
+    using System.Text.RegularExpressions;
 
     public class UrlHelpers
     {
+        private static readonly Regex slugCharacters = new Regex(@"([\s,.//\\-_=])+");
+
         /// <summary>
         /// Produces optional, URL-friendly version of a Topic, "like-this-one". 
-        /// hand-tuned for speed, reflects performance refactoring contributed
         /// </summary>
         public static string UrlFriendly(string title)
         {
-            if (title == null)
+            if (string.IsNullOrEmpty(title))
             {
                 return string.Empty;
             }
 
-            const int maxLength = 80;
-            var prevdash = false;
-            var sb = new StringBuilder(title.Length);
+            var newTitle = title.ToLowerInvariant();
+            newTitle = slugCharacters.Replace(newTitle, "-");
 
-            for (var i = 0; i < title.Length; i++)
-            {
-                var currentChar = title[i];
-
-                if ((currentChar >= 'a' && currentChar <= 'z') || (currentChar >= '0' && currentChar <= '9'))
-                {
-                    sb.Append(currentChar);
-                    prevdash = false;
-                }
-                else if (currentChar >= 'A' && currentChar <= 'Z')
-                {
-                    sb.Append((char)(currentChar | 32));
-                    prevdash = false;
-                }
-                else if (currentChar == ' ' ||
-                         currentChar == ',' ||
-                         currentChar == '.' ||
-                         currentChar == '/' ||
-                         currentChar == '\\' ||
-                         currentChar == '-' ||
-                         currentChar == '_' ||
-                         currentChar == '=')
-                {
-                    if (!prevdash && sb.Length > 0)
-                    {
-                        sb.Append('-');
-                        prevdash = true;
-                    }
-                }
-                else if ((int)currentChar >= 128)
-                {
-                    var prevlen = sb.Length;
-                    sb.Append(RemapInternationalCharToAscii(currentChar));
-
-                    if (prevlen != sb.Length)
-                    {
-                        prevdash = false;
-                    }
-                }
-
-                if (i == maxLength)
-                {
-                    break;
-                }
-            }
-
-            return prevdash
-                ? sb.ToString().Substring(0, sb.Length - 1)
-                : sb.ToString();
+            return RemoveDiacritics(newTitle);
         }
 
-        private static string RemapInternationalCharToAscii(char c)
+        private static string RemoveDiacritics(string text)
         {
-            var s = c.ToString().ToLowerInvariant();
+            var normalizedString = text.Normalize(NormalizationForm.FormD);
+            var sb = new StringBuilder();
 
-            if ("àåáâäãåą".Contains(s))
+            foreach (var c in normalizedString)
             {
-                return "a";
+                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
+
+                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(c);
+                }
             }
-            else if ("èéêëę".Contains(s))
-            {
-                return "e";
-            }
-            else if ("ìíîïı".Contains(s))
-            {
-                return "i";
-            }
-            else if ("òóôõöøőð".Contains(s))
-            {
-                return "o";
-            }
-            else if ("ùúûüŭů".Contains(s))
-            {
-                return "u";
-            }
-            else if ("çćčĉ".Contains(s))
-            {
-                return "c";
-            }
-            else if ("żźž".Contains(s))
-            {
-                return "z";
-            }
-            else if ("śşšŝ".Contains(s))
-            {
-                return "s";
-            }
-            else if ("ñń".Contains(s))
-            {
-                return "n";
-            }
-            else if ("ýÿ".Contains(s))
-            {
-                return "y";
-            }
-            else if ("ğĝ".Contains(s))
-            {
-                return "g";
-            }
-            else if (c == 'ř')
-            {
-                return "r";
-            }
-            else if (c == 'ł')
-            {
-                return "l";
-            }
-            else if (c == 'đ')
-            {
-                return "d";
-            }
-            else if (c == 'ß')
-            {
-                return "ss";
-            }
-            else if (c == 'Þ')
-            {
-                return "th";
-            }
-            else if (c == 'ĥ')
-            {
-                return "h";
-            }
-            else if (c == 'ĵ')
-            {
-                return "j";
-            }
-            else
-            {
-                return "";
-            }
+
+            return sb
+                .ToString()
+                .Normalize(NormalizationForm.FormC);
         }
+
     }
 }
