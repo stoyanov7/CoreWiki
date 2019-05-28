ea74b239b293fd7fd5e8c5981cc931cef49de178
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-05-28 14:44:42 +0300

    Implement list comment component

---
+++ src/CoreWiki.Web/Pages/Components/ListComments/ListComments.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using CoreWiki.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoreWiki.Web.Pages.Components.ListComments
+{
+    using Data;
+
+    [ViewComponent(Name = "ListComments")]
+    public class ListComments : ViewComponent
+    {
+        private readonly CoreWikiContext context;
+
+        public ListComments(CoreWikiContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(ICollection<Comment> comments)
+        {
+            return View("ListComments", comments);
+        }
+    }
+}
