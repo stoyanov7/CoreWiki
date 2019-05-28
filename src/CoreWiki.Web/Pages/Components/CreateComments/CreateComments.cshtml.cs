67230cb530cf968e2ff2047c1ac4775512be1b26
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-05-28 14:44:20 +0300

    Implement CreateComment component

---
+++ src/CoreWiki.Web/Pages/Components/CreateComments/CreateComments.cshtml.cs
@@ -0,0 +1,23 @@
+﻿namespace CoreWiki.Web.Pages.Components.CreateComments
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+    using Data;
+    using Models;
+
+    [ViewComponent(Name = "CreateComments")]
+    public class CreateComments : ViewComponent
+    {
+        private readonly CoreWikiContext context;
+
+        public CreateComments(CoreWikiContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(Comment comment)
+        {
+            return View("CreateComments", comment);
+        }
+    }
+}
