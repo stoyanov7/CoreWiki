8639e337d0e45e3e33066106f58afd277dd836c7
stoyanov7 <georgi.stoyanov777@gmail.com>
2019-06-10 15:42:40 +0300

    Create new class where all authentication policy will be stored

---
+++ src/CoreWiki.Utilities/AuthPolicy.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreWiki.Utilities
+{
+    using Microsoft.AspNetCore.Authorization;
+
+    public class AuthPolicy
+    {
+        public static void Execute(AuthorizationOptions options)
+        {
+
+        }
+    }
+}
