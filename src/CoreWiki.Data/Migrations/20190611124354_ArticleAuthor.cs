27324061e3a9d9bcc411ffe7fb2ab224ffbd2929
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-06-11 15:52:28 +0300

    Article have author

---
+++ src/CoreWiki.Data/Migrations/20190611124354_ArticleAuthor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoreWiki.Data.Migrations
+{
+    public partial class ArticleAuthor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AuthorId",
+                table: "Articles",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_AuthorId",
+                table: "Articles",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Articles_AspNetUsers_AuthorId",
+                table: "Articles",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Articles_AspNetUsers_AuthorId",
+                table: "Articles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Articles_AuthorId",
+                table: "Articles");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorId",
+                table: "Articles");
+        }
+    }
+}
--- src/CoreWiki.Models/Identity/ApplicationUser.cs
+++ src/CoreWiki.Models/Identity/ApplicationUser.cs
@@ -1,9 +1,10 @@
 ﻿namespace CoreWiki.Models.Identity
 {
+    using System.Collections.Generic;
     using Microsoft.AspNetCore.Identity;
 
     public class ApplicationUser : IdentityUser
     {
-        
+        public ICollection<Article> Articles { get; set; } = new HashSet<Article>();
     }
 }
