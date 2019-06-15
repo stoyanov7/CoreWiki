45d1f12194076d08350baeed3ef0ea7cb474fe60
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-06-15 13:46:45 +0300

    Create validation class for comment

---
+++ src/CoreWiki.Data/EntityConfiguration/CommentValidator.cs
@@ -0,0 +1,18 @@
+﻿namespace CoreWiki.Data.EntityConfiguration
+{
+    using FluentValidation;
+    using Models;
+
+    public class CommentValidator : AbstractValidator<Comment>
+    {
+        public CommentValidator()
+        {
+            this.RuleFor(o => o.Name)
+                .NotEmpty()
+                .MaximumLength(100);
+
+            this.RuleFor(o => o.Email)
+                .EmailAddress();
+        }
+    }
+}
