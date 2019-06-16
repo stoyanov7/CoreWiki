54b65960362925c7d2bc42f7a92745df9abf8c6f
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-06-16 14:21:55 +0300

    Create ArticleHistory

--- src/CoreWiki.Data/Migrations/20190615111339_ArticleHistory.Designer.cs
+++ src/CoreWiki.Data/Migrations/20190615111339_ArticleHistory.Designer.cs
@@ -4,14 +4,16 @@ using CoreWiki.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoreWiki.Data.Migrations
 {
     [DbContext(typeof(CoreWikiContext))]
-    partial class CoreWikiContextModelSnapshot : ModelSnapshot
+    [Migration("20190615111339_ArticleHistory")]
+    partial class ArticleHistory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- src/CoreWiki.Models/ArticleHistory.cs
+++ src/CoreWiki.Models/ArticleHistory.cs
@@ -1,18 +1,22 @@
 ﻿namespace CoreWiki.Models
 {
     using System;
-    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using Identity;
     using NodaTime;
     using NodaTime.Extensions;
 
-    public class Article
+    public class ArticleHistory
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        public int ArticleId { get; set; }
+        public Article Article { get; set; }
+
+        [Required]
+        public int Version { get; set; }
 
         [Required]
         [MaxLength(100)]
@@ -23,30 +27,17 @@
         [DataType(DataType.MultilineText)]
         public string Content { get; set; }
 
-        public long ViewCount { get; set; }
-
-        public string AuthorId { get; set; }
-        public ApplicationUser Author { get; set; }
-
-        [Required]
-        public int Version { get; set; } = 1;
-
         [NotMapped]
         public Instant Published { get; set; }
 
         [Obsolete("This property only exists for EF-serialization purposes")]
-        [DataType(DataType.Date)]
-        [Display(Name = "Published on")]
-        public DateTime PublishedOn
+        [DataType(DataType.DateTime)]
+        [Column("Published")]
+        public DateTime PublisheOn
         {
             get => this.Published.ToDateTimeUtc();
-
+            
             set => this.Published = DateTime.SpecifyKind(value, DateTimeKind.Utc).ToInstant();
         }
-
-        public virtual ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
-
-        public virtual ICollection<ArticleHistory> History { get; set; }
-            = new HashSet<ArticleHistory>();
     }
 }
