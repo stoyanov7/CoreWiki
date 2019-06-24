4e64f01419bfd4b739f606eb36446643dcd675ab
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-06-24 11:42:54 +0300

    Test Url helper

---
+++ test/CoreWiki.Utilities.Test/UrlHelperTests.cs
@@ -0,0 +1,18 @@
+﻿namespace CoreWiki.Utilities.Test
+{
+    using Xunit;
+
+    public class UrlHelperTests
+    {
+        [Theory]
+        [InlineData(null, "")]
+        [InlineData("", "")]
+        [InlineData("One Two", "one-two")]
+        public void TopicToSlug(string topic, string expectedSlug)
+        {
+            var createdSlug = UrlHelpers.UrlFriendly(topic);
+
+            Assert.Equal(expectedSlug, createdSlug);
+        }
+    }
+}
