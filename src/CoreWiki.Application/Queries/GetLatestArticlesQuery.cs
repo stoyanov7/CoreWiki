4cbfc89c96a3e672cb34d3fc4b8e3249a460993c
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-07-18 14:53:38 +0300

    Wrap get latest article with mediator

---
+++ src/CoreWiki.Application/Queries/GetLatestArticlesQuery.cs
@@ -0,0 +1,15 @@
+﻿namespace CoreWiki.Application.Queries
+{
+    using System.Collections.Generic;
+    using MediatR;
+
+    public class GetLatestArticlesQuery : IRequest<IEnumerable<LatestArticleDto>>
+    {
+        public GetLatestArticlesQuery(IEnumerable<LatestArticleDto> article)
+        {
+            this.Article = article;
+        }
+
+        public IEnumerable<LatestArticleDto> Article { get; set; }
+    }
+}
---
+++ src/CoreWiki.Application/Queries/GetLatestArticlesQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace CoreWiki.Application.Queries
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using MediatR;
+    using Services.Contracts;
+
+    public class GetLatestArticlesQueryHandler : IRequestHandler<GetLatestArticlesQuery, IEnumerable<LatestArticleDto>>
+    {
+        private readonly IArticleService articleService;
+
+        public GetLatestArticlesQueryHandler(IArticleService articleService)
+        {
+            this.articleService = articleService;
+        }
+
+        public async Task<IEnumerable<LatestArticleDto>> Handle(
+            GetLatestArticlesQuery request,
+            CancellationToken cancellationToken)
+        {
+            return await this.articleService.GetLatestArticle<LatestArticleDto>();
+        }
+    }
+}
