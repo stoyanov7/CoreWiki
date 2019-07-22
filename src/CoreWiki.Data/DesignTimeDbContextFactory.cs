7ef1fb1e10f908868fabb73bc8e2985b5a1c1e9b
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-07-22 15:29:04 +0300

    Create DesignTimeDbContextFactory for dotnet ef cli

---
+++ src/CoreWiki.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace CoreWiki.Data
+{
+    using System.IO;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.Extensions.Configuration;
+
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CoreWikiContext>
+    {
+        public CoreWikiContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<CoreWikiContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString);
+
+            return new CoreWikiContext(builder.Options);
+        }
+    }
+}
