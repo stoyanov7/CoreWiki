6587c56b4aa6bf1c6c47d21b1ba70874701d11e7
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-07-26 14:56:22 +0300

    Crete role service

---
+++ src/CoreWiki.Services/Contracts/IRoleService.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreWiki.Services.Contracts
+{
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Identity;
+
+    public interface IRoleService
+    {
+        ICollection<string> GetAllRoleNames();
+
+        IEnumerable<IdentityRole> GetAllRoles();
+    }
+}
---
+++ src/CoreWiki.Services/RoleService.cs
@@ -0,0 +1,26 @@
+﻿namespace CoreWiki.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Contracts;
+    using Microsoft.AspNetCore.Identity;
+
+    public class RoleService : IRoleService
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public RoleService(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+
+        public IEnumerable<IdentityRole> GetAllRoles() =>
+            this.roleManager.Roles.ToList();
+
+        public ICollection<string> GetAllRoleNames() =>
+            this.roleManager
+                .Roles
+                .Select(x => x.Name)
+                .ToList();
+    }
+}
