eec6e0f959aa9955c5c5412b6d502200625dc863
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-07-26 15:00:10 +0300

    Create roles pages to sholl all roles in table

--- src/CoreWiki.Web/Areas/Admin/Pages/Roles.cshtml.cs
+++ src/CoreWiki.Web/Areas/Admin/Pages/Roles.cshtml.cs
@@ -1,11 +1,47 @@
 namespace CoreWiki.Web.Areas.Admin.Pages
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Services.Contracts;
 
     public class RolesModel : PageModel
     {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IRoleService roleService;
+
+        public RolesModel(RoleManager<IdentityRole> roleManager, IRoleService roleService)
+        {
+            this.roleManager = roleManager;
+            this.roleService = roleService;
+        }
+
+        [BindProperty]
+        public string RoleToRemove { get; set; }
+
+        public IEnumerable<IdentityRole> RolesList { get; private set; }
+
         public void OnGet()
         {
+            this.RolesList = this.roleService.GetAllRoles();
+
+            this.Page();
+        }
+
+        public async Task<IActionResult> OnPostDeleteRoleAsync()
+        {
+            
+            var role = await this.roleManager.FindByNameAsync(this.RoleToRemove);
+            var result = await this.roleManager.DeleteAsync(role);
+
+            if (result.Succeeded)
+            {
+                return this.RedirectToPage();
+            }
+
+            return this.Page();
         }
     }
 }
