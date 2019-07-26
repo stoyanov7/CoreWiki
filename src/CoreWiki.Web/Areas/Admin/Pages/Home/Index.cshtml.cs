30140d99c04dfca4901c25069e1c85c42890dddf
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-07-26 15:40:13 +0300

    If user is Administrator the login redirect to admin area

---
+++ src/CoreWiki.Web/Areas/Admin/Pages/Home/Index.cshtml.cs
@@ -0,0 +1,11 @@
+namespace CoreWiki.Web.Areas.Admin.Pages.Home
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
