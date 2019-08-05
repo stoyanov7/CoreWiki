5863fb6dd35620e0c3ea70df1505b073ae3e6725
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-08-05 12:39:37 +0300

    Create DTO for binding, use HTML helpers and write content to appsettings

---
+++ src/CoreWiki.Web/Areas/FirstStart/FirstStartConfiguration.cs
@@ -0,0 +1,30 @@
+﻿namespace CoreWiki.Web.Areas.FirstStart
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class FirstStartConfiguration
+    {
+        [Required]
+        [StringLength(200, MinimumLength = 3)]
+        [Display(Name = "Administrator Username")]
+        public string AdminUsername { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Administrator Email Address")]
+        public string AdminEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = " Administrator Password")]
+        public string AdminPassword { get; set; }
+
+        [Required]
+        public string Database { get; set; }
+
+        [Required]
+        [Display(Name = "Connection String")]
+        public string ConnectionString { get; set; }
+    }
+}
--- src/CoreWiki.Web/Areas/FirstStart/Pages/Index.cshtml.cs
+++ src/CoreWiki.Web/Areas/FirstStart/Pages/Index.cshtml.cs
@@ -1,10 +1,83 @@
 namespace CoreWiki.Web.Areas.FirstStart.Pages
 {
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Models.Identity;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     public class IndexModel : PageModel
     {
-        public IActionResult OnGet() => this.Page();
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IHostingEnvironment hostingEnvironment;
+
+        public IndexModel(
+            UserManager<ApplicationUser> userManager,
+            IHostingEnvironment hostingEnvironment)
+        {
+            this.userManager = userManager;
+            this.hostingEnvironment = hostingEnvironment;
+        }
+
+        [BindProperty]
+        public FirstStartConfiguration FirstStartConfiguration { get; set; }
+
+        public IActionResult OnGet()
+        {
+            this.TestWritingFileToDisk();
+
+            return this.Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.Page();
+            }
+
+            var newAdminUser = new ApplicationUser
+            {
+                UserName = this.FirstStartConfiguration.AdminUsername,
+                Email = this.FirstStartConfiguration.AdminEmail
+            };
+
+            var userResult = await this.userManager
+                .CreateAsync(newAdminUser, this.FirstStartConfiguration.AdminPassword);
+
+            if (userResult.Succeeded)
+            {
+                var result = await this.userManager
+                    .AddToRoleAsync(newAdminUser, "Administrator");
+            }
+
+            return this.RedirectToPage("./Index", new { Area = "" });
+        }
+
+        private void TestWritingFileToDisk()
+		{
+			var settingsFileLocation = Path.Combine(this.hostingEnvironment.ContentRootPath, "appsettings.json");
+
+			if (!System.IO.File.Exists(settingsFileLocation))
+            {
+				var fileStream = System.IO.File.Create(settingsFileLocation);
+				var bytes = Encoding.ASCII.GetBytes("{}");
+				fileStream.Write(bytes, 0, bytes.Length);
+				fileStream.Close();
+				fileStream.Dispose();
+			}
+
+			var fileContents = System.IO.File.ReadAllText(settingsFileLocation);
+
+			var jsonFile = JsonConvert.DeserializeObject<JObject>(fileContents);
+            jsonFile["foo"] = "bar";
+
+			System.IO.File.WriteAllText(settingsFileLocation, JsonConvert.SerializeObject(jsonFile, Formatting.Indented));
+		}
     }
 }
