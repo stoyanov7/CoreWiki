6929be76fda9a0d5bc1918bfa60aeae479929be1
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2019-09-09 12:35:53 +0300

    Implement class to check if the given password is pwned

---
+++ src/CoreWiki.Domain/Services/IHaveIBeenPawnedClient.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreWiki.Domain.Services
+{
+    using System.Threading.Tasks;
+
+    public interface IHaveIBeenPawnedClient
+    {
+        Task<int> GetHitsPlainAsync(string password);
+    }
+}
